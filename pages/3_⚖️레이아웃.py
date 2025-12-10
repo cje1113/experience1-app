59b38e50bf878224b8608cca32d92bc4b01b88fe
cje1113 <cje33333@naver.com>
2025-12-10 19:13:01 +0900

    update

--- pages/3_⚖️레이아웃.py
+++ pages/3_⚖️레이아웃.py
@@ -15,3 +15,49 @@ with col_2:
 
 col_3.write('## 3번 컬럼')
 col_3.selectbox('3번 컬럼의 셀렉트박스', ['select 1', 'select 2', 'select 3'])
+
+'### :orange[탭: st.tabs()]'
+
+# 탭 인스턴스 3개 생성
+tab_1, tab_2, tab_3 = st.tabs(['Python', 'R', 'Julia'])
+with tab_1:
+    st.write(
+        '''
+        ``` python
+        import pandas as pd
+
+        df = pd.DataFrame(
+            {'id': [1,2,3,],
+            'name': ['Alice', 'Bob', 'Charlie'],
+            'age': [24, 34, 45]
+            }
+        )
+        ```
+        '''
+    )
+
+with tab_2:
+    st.write(
+        '''
+        ``` r
+        df <- data.frame(
+            id = c(1,2,3),
+            name = c('Alice', 'Bob', 'Charlie'),
+            age = c(24, 34, 45)
+        )
+        ```
+        '''
+    )
+
+tab_3.write(
+    '''
+    ``` julia
+    using DataFrames
+
+    df = DataFrame(
+        id = [1,2,3],
+        name = ['Alice', 'Bob', 'Charlie'],
+        age = [24, 34, 45])
+    ```
+    '''
+)
