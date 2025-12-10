8670523fa99c3ea48087c07f17826662266343e0
cje1113 <cje33333@naver.com>
2025-12-10 18:16:57 +0900

    update

--- pages/2_📈데이터_그래프.py
+++ pages/2_📈데이터_그래프.py
@@ -8,9 +8,21 @@ df = pd.DataFrame(
 df # 데이터프레임 출력
 
 '### :orange[지표(Metric)]'
-import steamlit as st
+import streamlit as st
 col1, col2, col3 = st.columns(3)    # 3개 컬럼 생성
 # st.metric(label -> 무엇, value -> 현재값, delta -> 변화량(증가면 초록, 감소면 빨강)
 col1.metric("Temperature", '70 F', '1.2 F')
 col2.metric('Wind', '9 mph', '-8%')
 col3.metric('Humidity', '86%', '4%')
+
+'# :blue[Streamlit 그래프]'
+import pandas as pd
+import numpy as np
+
+chart_data = pd.DataFrame(
+    np.random.randn(20,3),  # 표준정규분포(평균 0, 표준편차 1) 를 따르는 20행 3열 짜리 난수 행렬
+    columns=['a','b','c']
+)
+
+'### :orange[st.area_chart()]'
+st.area_chart(chart_data)
