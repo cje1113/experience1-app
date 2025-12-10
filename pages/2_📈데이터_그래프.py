dac60f70b1cd14bb198a382f0c221470c5243fd6
cje1113 <cje33333@naver.com>
2025-12-10 18:12:49 +0900

    update

--- pages/2_📈데이터_그래프.py
+++ pages/2_📈데이터_그래프.py
@@ -6,3 +6,11 @@ df = pd.DataFrame(
      'age': [24, 34, 45]}
 )
 df # 데이터프레임 출력
+
+'### :orange[지표(Metric)]'
+import steamlit as st
+col1, col2, col3 = st.columns(3)    # 3개 컬럼 생성
+# st.metric(label -> 무엇, value -> 현재값, delta -> 변화량(증가면 초록, 감소면 빨강)
+col1.metric("Temperature", '70 F', '1.2 F')
+col2.metric('Wind', '9 mph', '-8%')
+col3.metric('Humidity', '86%', '4%')
