a6aba7c2ecb6840faf4bdbd28c7d3c081c3af1bb
cje1113 <cje33333@naver.com>
2025-12-10 20:10:10 +0900

    update

---
+++ pages/5_🚀고급기능.py
@@ -0,0 +1,15 @@
+import time
+import streamlit as st
+
+@st.cache_data
+def long_running_function(param1):
+    time.sleep(5)
+    return param1 * param1
+
+start = time.time()
+
+# 숫자 입력은 입력된 값을 반환
+num_1 = st.number_input('입력한 숫자의 제곱을 계산합니다.')
+st.write(f'{num_1}의 제곱은 {long_running_function} 입니다.' +
+         f'계산시간은 {time.time() - start:.2f}초 소요')
+st.write('🚀 :green[캐싱이 적용되면 동일한 계산은 저장된 결과를 사용해 빠르게 처리]')
